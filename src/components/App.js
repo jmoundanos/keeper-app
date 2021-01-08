f3046c0b78d737c6a772666bb8851d7f90e83a22
Jennifer Moundanos <jmoundanos@gmail.com>
2021-01-07 20:56:07 -0600

    add note function

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,21 @@ import { BrowserRouter as Router, Route, Switch } from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-
+import notes from "../notes";
 
 class App extends Component {
     render(){
         return(
             <div>
                 <Header />
-                <Note />
+                {notes.map(noteItem => 
+                    <Note 
+                    key = {noteItem.id}
+                    title = {noteItem.title}
+                    content = {noteItem.content}
+                    />
+                )}
+                
                 <Footer />
             </div>
         )
