af79c0631571a1f11f9622c77634981f896d7299
Jennifer Moundanos <jmoundanos@gmail.com>
2021-02-16 14:44:40 -0600

    add note taking area

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+
+import CreateNote from "./CreateNote";
 
 class App extends Component {
     render(){
         return(
             <div>
                 <Header />
-                {notes.map(noteItem => 
-                    <Note 
-                    key = {noteItem.id}
-                    title = {noteItem.title}
-                    content = {noteItem.content}
-                    />
-                )}
-                
+                <CreateNote />
+                <Note 
+                    key={1}
+                    title="Note title"
+                    content="Content"
                 <Footer />
             </div>
         )
