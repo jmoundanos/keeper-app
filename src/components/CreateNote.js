1fa9125fcfb5f35a3ad1cc4316e2ac4539111e7b
Jennifer Moundanos <jmoundanos@gmail.com>
2021-02-25 16:25:41 -0600

    add function to create notes

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function CreateNote(){
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    })
+    function handleChange(event){
+    const { name, value } = event.target;
+    setNote(prevNote =>{
+        return{
+            ...prevNote,
+            [name]: value
+        };
+    });
+    }
     return (
         <div>
             <form>
-            <input name="title" placeholder="Title" />
-            <textarea name="content" placeholder="Take a note" />
+            <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />
+            <textarea onChange={handleChange} name="content" value={note.content} placeholder="Take a note" />
             <button>Add</button>
             </form>
         </div>
